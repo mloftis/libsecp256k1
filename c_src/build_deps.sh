853fd13b8c5aafba5a19a26d69782ad2a08a2f25
Michael Loftis <mloftis@wgops.com>
2018-06-11 20:02:50 +0000

    attemp to uprev secp256k1 build fails miserably on Debian Stretch (openssl 1.1)

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -14,7 +14,7 @@ case "$1" in
 
     (test -d secp256k1 || git clone https://github.com/bitcoin/secp256k1)
 
-    (cd secp256k1 && git reset --hard 5a91bd768faaa974e00301e662fd8f2aa75a122a &&  ./autogen.sh && ./configure --enable-module-recovery && make)
+    (cd secp256k1 && git reset --hard 1e6f1f5ad5e7f1e3ef79313ec02023902bf8175c &&  ./autogen.sh && ./configure --enable-module-recovery && make)
 	#(cd secp256k1 &&  ./autogen.sh && ./configure --enable-module-recovery && make)
     ;;
 esac
